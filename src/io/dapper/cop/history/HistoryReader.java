1b46bd81b820ad513fd10d0c62882363ee972b3f
daviduvalle <david.uvalle@gmail.com>
2018-08-23 21:35:14 -0700

    Cleaned file

--- src/io/dapper/cop/history/HistoryReader.java
+++ src/io/dapper/cop/history/HistoryReader.java
@@ -1,34 +1,36 @@
 package io.dapper.cop.history;
 
-import io.dapper.cop.configuration.CopConfiguration;
 import io.dapper.cop.models.TestInstance;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.lang.reflect.Type;
 import java.util.stream.Collectors;
 
-import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 
 /**
- *  Reads a netcop history file
- *  if it exists
+ *  Reads a netcop history file if it exists
  */
 public class HistoryReader {
 
     private final File tmpFile;
 
+    /**
+     * Reads a netcope history file
+     * @param tmpFile file to read from
+     */
     public HistoryReader(File tmpFile) {
         this.tmpFile = tmpFile;
     }
 
+    /**
+     * Reads a netcop history file and returns a list of test instances
+     * @return a list of test instances
+     * @throws IOException if unable to read file contents
+     */
     public List<TestInstance> read() throws IOException {
 
         // Reads a file containing different samples stored as separate
